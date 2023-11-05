2a5f049222e81bc12b5333777f9d545a05791dd6
laith17 <laithalmajaly007@gmail.com>
2023-11-05 21:28:15 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './Component/Website/Cart';
+import Home from './Component/Website/Home';
+
+import Checkout from './Component/Website/Checkout';
+
+import Registration from './Component/Website/Registration';
+import Payment from './Component/Website/PaymentN';
+import Login from './Component/Website/Login';
+import Wishlist from './Component/Website/Wishlist';
+import FAQ from './Component/Website/FAQ';
+import ContactUs from './Component/Website/ContactUs';
+import AboutUs from './Component/Website/AboutUs';
+import AdsBar from './Component/Home/AdsBar';
+import { Navbar } from '@material-tailwind/react';
+import { useState } from 'react';
+import Footer from './Component/Website/Footer';
+import Navbar2 from './Component/Website/Navbar2';
+import Profile from './Component/Website/Profile';
+import Product from './Component/Website/ProductPages';
+import Details from './Component/Website/Details';
+
+
+
 
 function App() {
+
+  const [cartItems, setCartItems] = useState([]);
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+ 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <AdsBar/>
+   
+     <Navbar2 cartItems={cartItems} onToggleMobileNav={toggleMobileNav} totalAmount={totalAmount}/>
+     {isMobileNavOpen && <div className="overlay" onClick={toggleMobileNav} />}
+   
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        
+          
+          <Route path='/cart' element={<Cart/>}/>
+          {/* <Route path='/checkout/:id' element={<Checkout/>}/> */}
+          <Route path='/registration' element={<Registration/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/favorite' element={<Wishlist/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/payment' element={<Payment/>}/>
+          <Route path='/contact' element={<ContactUs/>}/>
+          <Route path='/about' element={<AboutUs/>}/>
+          <Route path='/faq' element={<FAQ/>}/>
+          <Route path='/productDetails' element={<Product/>}/>
+          <Route path="/Details/:Params" element={<Details />} />
+       
+          
+           
+        </Routes> <Footer/>
+      </Router>
+    
+     
     </div>
   );
 }
